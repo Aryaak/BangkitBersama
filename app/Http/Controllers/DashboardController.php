e6e2de57fa3793c98223568d7e7846e467900a33
Cinta Ramayanti <cinta.ramayanti@gmail.com>
2021-10-14 19:50:03 +0700

    bagian2 dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,13 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
+use App\Models\Help;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard');
+        $user= User::all()->count();
+        $covid = Help::all()->where('help_category_id', 1)->count();
+        $ekonomi = Help::all()->where('help_category_id', 2)->count();
+        $pangan = Help::all()->where('help_category_id', 3)->count();
+        $jasa = Help::all()->where('help_category_id', 4)->count();
+
+        return view('pages.dashboard', ['user' => $user, 'covid' => $covid, 'ekonomi' => $ekonomi, 'pangan' => $pangan, 'jasa' => $jasa
+    ]);
     }
 }
